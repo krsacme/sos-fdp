d37a2d752458d717b8e498dbe19dbd16fdd37071
Aaron Smith <aasmith@redhat.com>
2020-08-24 11:02:22 -0400

    working

--- fdp.sh
+++ fdp.sh
@@ -8,6 +8,18 @@ export OS_CLOUD=openstack
 
 export KUBECONFIG="$BUILD_DIR/auth/kubeconfig"
 
+declare -a network_cmds=(
+    "openstack network create --share --provider-network-type flat --provider-physical-network external --external public"
+    "openstack subnet create --no-dhcp --allocation-pool start=192.168.122.125,end=192.168.122.200\\
+      --gateway 192.168.122.1 --subnet-range 192.168.122.0/24 --network public public_subnet"
+    "openstack router create public"
+    "router set --external-gateway public public"
+    "subnet create ocp --network ocp --subnet-range 10.0.0.0/16 --dhcp"
+    "router add subnet public ocp"
+    "openstack floating ip create --floating-ip-address 192.168.122.150 --description "API fdp.nfv" public"
+    "openstack floating ip create --floating-ip-address 192.168.122.151 --description "Ingress fdp.nfv" public"
+)
+
 usage() {
     local out_dir="$1"
 
@@ -48,20 +60,20 @@ etcd_hack() {
     touch f0
 
     wait_for_log
-    
+
     touch f1
 
     while IFS= read -r line; do
-        echo "$line" >>  f3
+        echo "$line" >>f3
         [[ "$VERBOSE" =~ true ]] && echo "$line"
         touch f4
 
         if [[ ${line} =~ "for bootstrapping to complete" ]]; then
-          echo "$line" > f5          
-          break;
+            echo "$line" >f5
+            break
         fi
     done | tail -f "$BUILD_LOG"
-    
+
     touch f2
 
     oc patch etcd cluster -p='{"spec": {"unsupportedConfigOverrides": {"useUnsupportedUnsafeNonHANonProductionUnstableEtcd": true}}}' --type=merge
@@ -83,12 +95,98 @@ deploy_cluster() {
     cp metadata.json "$ARTIFACTS_DIR" || return 1
 
     # Queue up tasks to run according to the stage in deployment
-    etcd_hack &
+    # etcd_hack &
 
     openshift-install create cluster --log-level debug
 
     # Wait for hacks to finish
-    wait 
+    wait
+}
+
+#
+# pre -> Precheck command.  If true cmd will not be executed
+# cmd -> command to be executed
+# post -> command to check if successful
+#
+run_cmd() {
+    local pre="$1"
+    local cmd="$2"
+    local post="$2"
+
+    # if there is precheck command, run it.
+    # if successful, return
+    if [ -n "$pre" ] && "$pre"; then
+        return 1
+    fi
+
+    if [ -z "$cmd" ]; then
+        return 0
+    fi
+
+    if ! "$cmd"; then
+        printf "Error: Execution failed: %s!\n" "$cmd"
+        return 1
+    fi
+
+    if [ -z "$post" ]; then
+        return 0
+    fi
+
+    if ! "$post"; then
+        printf "Error: Execution failed: %s!\n" "$cmd"
+        return 1
+    fi
+
+}
+
+setup() {
+    declare -a network_cmds=(
+        "openstack network create --share --provider-network-type flat --provider-physical-network external --external public"
+        "openstack subnet create --no-dhcp --allocation-pool start=192.168.122.125,end=192.168.122.200\\
+      --gateway 192.168.122.1 --subnet-range 192.168.122.0/24 --network public public_subnet"
+        "openstack router create public"
+        "openstack router set --external-gateway public public"
+        "openstack subnet create ocp --network ocp --subnet-range 10.0.0.0/16 --dhcp"
+        "openstack router add subnet public ocp"
+        "openstack floating ip create --floating-ip-address 192.168.122.150 --description "API fdp.nfv" public"
+        "openstack floating ip create --floating-ip-address 192.168.122.151 --description "Ingress fdp.nfv" public"
+    )
+    run_cmd "openstack network show public" \
+        "openstack network create --share --provider-network-type flat --provider-physical-network external --external public" \
+        "openstack network show public"
+
+    run_cmd "openstack subnet show public" \
+        "openstack subnet create --no-dhcp --allocation-pool start=192.168.122.125,end=192.168.122.200\\
+         --gateway 192.168.122.1 --subnet-range 192.168.122.0/24 --network public public" \
+        "openstack subnet show public"
+
+    run_cmd "openstack network show ocp" \
+        "openstack network create ocp" \
+        "openstack network show ocp"
+
+    run_cmd "openstack subnet show ocp" \
+        "openstack subnet create ocp --network ocp --subnet-range 10.0.0.0/16 --dhcp" \
+        "openstack router show public | grep -q -e '.*external_gateway_info.*|.*network_id'"
+
+    run_cmd "openstack router show public" \
+        "openstack router create public" \
+        "openstack router show public"
+
+    run_cmd "openstack router show public | grep -q -e '.*external_gateway_info.*|.*network_id'" \
+        "openstack router set --external-gateway public public" \
+        "openstack router show public | grep -q -e '.*external_gateway_info.*|.*network_id'"
+
+    run_cmd "openstack router show public | grep -q -e '.*interfaces_info.*|.*port_id'" \
+        "openstack router add subnet public ocp" \
+        "openstack router show public | grep -q -e '.*interfaces_info.*|.*port_id'"
+
+
+
+    if ! openstack flavor create --ram 16384 --disk 25 --vcpus 8 --property hw:cpu_policy=dedicated --property hw:mem_page_size=1GB ocp; then
+        printf "Error: Failed to create OCP flavor!"
+        return 1
+    fi
+
 }
 
 manage_cluster() {
@@ -98,16 +196,21 @@ manage_cluster() {
     deploy)
         rm -rf "$BUILD_DIR"
         mkdir -p "$BUILD_DIR"
-        if [ ! -e ./clouds.yaml ]; then
-            echo "Error: missing clouds.yaml file!"
+        if [ ! -e "$PROJECT_DIR/clouds.yaml" ]; then
+            echo "Error: missing $PROJECT_DIR/clouds.yaml file!"
+            return 1
+        fi
+        if [ ! -e "$PROJECT_DIR/secure.yaml" ]; then
+            echo "Error: missing $PROJECT_DIR/secure.yaml file!"
             return 1
         fi
         if [ ! -e ./install-config.yaml ]; then
             echo "Error: missing install-config.yaml file!"
             return 1
         fi
-        cp ./clouds.yaml "$BUILD_DIR"
-        cp ./install-config.yaml "$BUILD_DIR"
+        cp "$PROJECT_DIR/clouds.yaml" "$BUILD_DIR"
+        cp "$PROJECT_DIR/secure.yaml" "$BUILD_DIR"
+        cp "$PROJECT_DIR/install-config.yaml" "$BUILD_DIR"
 
         deploy_cluster
         ;;
@@ -226,6 +329,9 @@ destroy)
     SUB_COMMAND=$1
     shift
     destroy "$SUB_COMMAND"
+    ;;
+setup)
+    setup
     ;;
 *)
     echo "Unknown command: $COMMAND"
