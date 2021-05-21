ed87d51d470c0f2dacec7aaad7609e29326d9df4
Saravanan KR <skramaja@redhat.com>
2021-05-21 06:34:32 +0530

    Make csr-approve sync to allow it to wait for the node ready

--- csr-approve.sh
+++ csr-approve.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+set -ex
+
+WORKERS=$1
+if [ -z ${WORKERS} ]; then
+  echo "Provide number of worker nodes"
+  exit 1
+fi
+
 while true; do
+  READY_NODES=$(oc get nodes --selector node-role.kubernetes.io/worker="" -o json | jq -r '.items[] | select(.status.conditions[] | select(.type=="Ready" and .status=="True")) | .metadata.name' | wc -l)
+  if [[ "${WORKERS}" == "${READY_NODES}" ]]; then
+    exit 0
+  fi
   if oc get csr 2>/dev/null | grep -q Pending; then
     oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | xargs oc adm certificate approve
   fi
