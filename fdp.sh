58d7beec02ef0a96e45848f3ed29ba973b440289
Saravanan KR <skramaja@redhat.com>
2021-07-06 14:16:20 +0530

    Increase CPU and memory for worker node

--- fdp.sh
+++ fdp.sh
@@ -317,7 +317,7 @@ prepare_openstack() {
 
     printf "Create ocp flavor %s...\n" "$WORKER_FLAVOR"
     openstack flavor show "$WORKER_FLAVOR" >/dev/null 2>&1 || {
-        openstack flavor create --ram 12288 --disk 25 --vcpus 6 --property hw:cpu_policy=dedicated --property hw:mem_page_size=1GB "$WORKER_FLAVOR" || {
+        openstack flavor create --ram 20480 --disk 25 --vcpus 12 --property hw:cpu_policy=dedicated --property hw:mem_page_size=1GB "$WORKER_FLAVOR" || {
             printf "Failed to create ocp flavor.."
             exit 1
         }
